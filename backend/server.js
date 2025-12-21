820386a7a97e4228910a23e65358ba71c028caf2
nirmal-blip <nirmal.joshi.23co@iitram.ac.in>
2025-12-21 10:14:01 +0530

    done things in backend 1

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
 const logger = require('./src/utils/logger');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`);
--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL ,
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 app.use(cookieParser());
